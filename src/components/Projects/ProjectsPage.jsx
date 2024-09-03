1f467ef8c71544a8f63845c68489d496a3cba161
MrPatil83 <avinashpatil142001@gmail.com>
2024-09-04 01:11:06 +0530

    fully changes

--- src/components/Projects/ProjectsPage.jsx
+++ src/components/Projects/ProjectsPage.jsx
@@ -2,69 +2,82 @@
 import ProjectsCard from "./ProjectsCard"
 import { motion } from 'framer-motion';
 import { FadeRight } from "../../Animation";
+import project from "../../../assets/projects/project.png";
+import selectivesystemsproject from "../../../assets/projects/selectivesystemsproject.png";
+import Risealloys from "../../../assets/projects/Risealloys.png";
+import Steadlersandscales from "../../../assets/projects/Steadlersandscales.png";
+import Solwin from "../../../assets/projects/Solwin.png";
+import node from "../../../assets/skills/node.png"
+import react from "../../../assets/skills/react.png";
+import Tailwind from "../../../assets/skills/Tailwind.png";
+import typescript from "../../../assets/skills/typescript.png";
+import figma from "../../../assets/skills/figma.png";
+import mongodb from "../../../assets/skills/mongodb.png";
+import googleappscript from "../../../assets/skills/googleappscript.png";
+import css from "../../../assets/skills/css.png";
 
 const ProjectsPage = () => {
   const projects = [
     {
-      imgSrc: "/assets/projects/project.png",
+      imgSrc: project,
       title: "Imperio Railing Systems",
       description: "Designed and developed the official website for Imperio Railing Systems, showcasing their premium railing solutions with a modern, user-friendly interface.",
       skills: [
-        "/assets/skills/react.png",
-        "/assets/skills/typescript.png",
-        "/assets/skills/Tailwind.png",
-        "/assets/skills/figma.png",
+        react,
+        typescript,
+        Tailwind,
+        figma,
       ],
       demoLink: "https://www.imperiorailing.com/",
       sourceLink: "/",
     },
     {
-      imgSrc: "/assets/projects/selectivesystemsproject.png",
+      imgSrc: selectivesystemsproject,
       title: "Selective Systems",
       description: "Designed and developed the official website for Selective Systems, showcasing their premium windows solutions with a modern, user-friendly interface.",
       skills: [
-        "/assets/skills/react.png",
-        "/assets/skills/Tailwind.png",
-        "/assets/skills/mongodb.png",
-        "/assets/skills/node.png",
+        react,
+        Tailwind,
+        mongodb,
+        node,
       ],
       demoLink: "https://selectivesystems.in/",
       sourceLink: "/",
     },
     {
-      imgSrc: "/assets/projects/Risealloys.png",
+      imgSrc: Risealloys,
       title: "Rise Alloys",
       description: "Crafted and launched the official website for Rise Alloys, highlighting their top-tier aluminum solutions through a sleek, intuitive, and user-centric design.",
       skills: [
-        "/assets/skills/react.png",
-        "/assets/skills/css.png",
-        "/assets/skills/googleappscript.png",
+        react,
+        css,
+        googleappscript,
       ],
       demoLink: "https://risealloys.com/",
       sourceLink: "/",
     },
     {
-      imgSrc: "/assets/projects/Steadlersandscales.png",
+      imgSrc:Steadlersandscales,
       title: "Steadlers & Scales",
       description: "Explore our innovative project with Steadlers & Scales Interior Design. Check out the ongoing website to see our latest designs and updates.",
       skills: [
-        "/assets/skills/react.png",
-        "/assets/skills/Tailwind.png",
-        "/assets/skills/mongodb.png",
-        "/assets/skills/node.png",
+        react,
+        Tailwind,
+        mongodb,
+        node,
       ],
       demoLink: "https://interiorswebsite.vercel.app",
       sourceLink: "/",
     },
     {
-      imgSrc: "/assets/projects/Solwin.png",
+      imgSrc: Solwin,
       title: "Solwin Enterprises",
       description: "Explore our project for Solwin Enterprises, a top player in home automation IT solutions. The site is in progress, featuring advanced technology for smarter living.",
       skills: [
-        "/assets/skills/react.png",
-        "/assets/skills/Tailwind.png",
-        "/assets/skills/mongodb.png",
-        "/assets/skills/node.png",
+        react,
+        Tailwind,
+        mongodb,
+        node,
       ],
       demoLink: "https://solwinenterprises.vercel.app/",
       sourceLink: "/",
