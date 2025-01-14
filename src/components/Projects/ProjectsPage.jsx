a857bc3c3f3cb35084323ee117a9f655c7766baf
MrPatil83 <avinashpatil142001@gmail.com>
2025-01-14 19:02:19 +0530

    new

--- src/components/Projects/ProjectsPage.jsx
+++ src/components/Projects/ProjectsPage.jsx
@@ -1,6 +1,6 @@
 // import React from "react";
-import ProjectsCard from "./ProjectsCard"
-import { motion } from 'framer-motion';
+import ProjectsCard from "./ProjectsCard";
+import { motion } from "framer-motion";
 import { FadeRight } from "../../Animation";
 import project from "../../../assets/projects/project.png";
 import selectivesystemsproject from "../../../assets/projects/selectivesystemsproject.png";
@@ -8,7 +8,7 @@ import Risealloys from "../../../assets/projects/Risealloys.png";
 import Rajgurusteel from "../../../assets/projects/Rajgurusteel.png";
 import Steadlersandscales from "../../../assets/projects/Steadlersandscales.png";
 import Solwin from "../../../assets/projects/Solwin.png";
-import node from "../../../assets/skills/node.png"
+import node from "../../../assets/skills/node.png";
 import react from "../../../assets/skills/react.png";
 import Tailwind from "../../../assets/skills/Tailwind.png";
 import typescript from "../../../assets/skills/typescript.png";
@@ -23,76 +23,55 @@ const ProjectsPage = () => {
     {
       imgSrc: project,
       title: "Imperio Railing Systems",
-      description: "Designed and developed the official website for Imperio Railing Systems, showcasing their premium railing solutions with a modern, user-friendly interface.",
-      skills: [
-        react,
-        typescript,
-        Tailwind,
-        figma,
-      ],
+      description:
+        "Designed and developed the official website for Imperio Railing Systems, showcasing their premium railing solutions with a modern, user-friendly interface.",
+      skills: [react, typescript, Tailwind, figma],
       demoLink: "https://www.imperiorailing.com/",
       sourceLink: "/",
     },
     {
       imgSrc: selectivesystemsproject,
       title: "Selective Systems",
-      description: "Designed and developed the official website for Selective Systems, showcasing their premium windows solutions with a modern, user-friendly interface.",
-      skills: [
-        react,
-        Tailwind,
-        mongodb,
-        node,
-      ],
+      description:
+        "Designed and developed the official website for Selective Systems, showcasing their premium windows solutions with a modern, user-friendly interface.",
+      skills: [react, Tailwind, mongodb, node],
       demoLink: "https://selectivesystems.in/",
       sourceLink: "/",
     },
     {
       imgSrc: Risealloys,
       title: "Rise Alloys",
-      description: "Crafted and launched the official website for Rise Alloys, highlighting their top-tier aluminum solutions through a sleek, intuitive, and user-centric design.",
-      skills: [
-        react,
-        css,
-        googleappscript,
-      ],
+      description:
+        "Crafted and launched the official website for Rise Alloys, highlighting their top-tier aluminum solutions through a sleek, intuitive, and user-centric design.",
+      skills: [react, css, googleappscript],
       demoLink: "https://risealloys.com/",
       sourceLink: "/",
     },
     {
       imgSrc: Rajgurusteel,
       title: "Rajguru Steel Industry",
-      description: "Explore our innovative project with Rajguru Steel Industry. Check out our ongoing website to see our latest stainless steel railing designs and updates. Stay tuned for more!",
-      skills: [
-        mongodb,
-        Next,
-      ],
+      description:
+        "Explore our innovative project with Rajguru Steel Industry. Check out our ongoing website to see our latest stainless steel railing designs and updates. Stay tuned for more!",
+      skills: [mongodb, Next],
       demoLink: "https://rajgurusteel.vercel.app/",
       sourceLink: "/",
     },
     {
-      imgSrc:Steadlersandscales,
+      imgSrc: Steadlersandscales,
       title: "Steadlers & Scales",
-      description: "Explore our innovative project with Steadlers & Scales Interior Design. Check out the ongoing website to see our latest designs and updates.",
-      skills: [
-        react,
-        Tailwind,
-        mongodb,
-        node,
-      ],
+      description:
+        "Explore our innovative project with Steadlers & Scales Interior Design. Check out the ongoing website to see our latest designs and updates.",
+      skills: [react, Tailwind, mongodb, node],
       demoLink: "https://interiorswebsites.vercel.app",
       sourceLink: "/",
     },
     {
       imgSrc: Solwin,
       title: "Solwin Enterprises",
-      description: "Explore our project for Solwin Enterprises, a top player in home automation IT solutions. The site is in progress, featuring advanced technology for smarter living.",
-      skills: [
-        react,
-        Tailwind,
-        mongodb,
-        node,
-      ],
-      demoLink: "https://solwinenterprises.vercel.app/",
+      description:
+        "Explore our project for Solwin Enterprises, a top player in home automation IT solutions. The site is in progress, featuring advanced technology for smarter living.",
+      skills: [react, Tailwind, mongodb, node],
+      demoLink: "https://solwinweb.vercel.app/",
       sourceLink: "/",
     },
   ];
